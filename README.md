d6d1dc01d2857f9ed628054fb1130dc49357b472
Weixuan Fu <weixuanf@outlook.com>
2024-04-12 14:51:05 +0800

    cleanup code

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Comfyui Serverless - a static comfyui serverless web interface
 
-Live at comfyui.nodeface.org 
+Live at [comfyui.nodeface.org](https://comfyui.nodecafe.org/)
 
-more coming soon!
+## Credits
 
+Sincerely thanks to these open source projects:
+
+ComfyUI: https://github.com/comfyanonymous/ComfyUI
+
+ComfyUI-Manager for nodes list and install scripts reference: https://github.com/ltdrdata/ComfyUI-Manager
--- scanner/githubUtils.py
+++ scanner/githubUtils.py
@@ -1,55 +1,5 @@
-import mimetypes
 import subprocess
 import requests
-import os
-import shutil
-
-github_key = os.environ.get('GITHUB_API_KEY')
-
-def get_github_repo_stars(repo_url):
-    if github_key is None:
-        print('🔴🔴 no github api key provided!!')
-    # Extracting the owner and repo name from the URL
-    parts = repo_url.split("/")
-    owner, repo = parts[-2], parts[-1]
-
-    # GitHub API endpoint for fetching repo details
-    api_url = f"https://api.github.com/repos/{owner}/{repo}"
-    
-    headers = {
-        "Authorization": f"token {github_key}",  # Adding the authentication token to the request header
-        "Accept": "application/vnd.github.v3+json"
-    }
-
-    # Making the authenticated request
-    response = requests.get(api_url, headers=headers)
-    if response.status_code == 200:
-        # Extracting the number of stars
-        repo_data = response.json()
-        stars = repo_data.get("stargazers_count", 0)
-        owner_avatar_url = repo_data['owner']['avatar_url']
-        # image_url = get_first_image_url_from_readme(owner, repo)
-        description = repo_data.get('description')
-        if not description:
-            # Fetch README content
-            readme_url = f"https://api.github.com/repos/{owner}/{repo}/readme"
-            readme_info = requests.get(readme_url, headers=headers).json()
-            readme_content = readme_info.get('content', '')
-            readme_text = requests.get(readme_info['download_url']).text if 'download_url' in readme_info else ""
-            paragraphs = readme_text.split('\n\n')
-            # Keep only the first two paragraphs
-            first_two_paragraphs = paragraphs[:2]
-            description = '\n'.join(first_two_paragraphs)
-
-        return {
-            "stars": stars,
-            "owner_avatar_url": owner_avatar_url,
-            'description': description,
-            # "image_url": image_url
-        }
-    else:
-        print(f"Failed to retrieve repository data. Status Code: {response.status_code}")
-        return {}
 import base64
 import re
 
@@ -77,34 +27,6 @@ def get_first_image_url_from_readme(owner, repo):
             return "No image found in README"
     else:
         return "Error: Unable to fetch README information"
-
-def clear_except_allowed_folder(path, allowedFolder):
-    """
-    Clears everything in the specified path except for the allowedFolder.
-    
-    :param path: Path to the directory to clear.
-    :param allowedFolder: The name of the folder to keep.
-    """
-    # Make sure the path is a directory
-    if not os.path.isdir(path):
-        print(f"The provided path {path} is not a directory.")
-        return
-
-    # Iterate through items in the directory
-    for item in os.listdir(path):
-        item_path = os.path.join(path, item)
-        # Check if the current item is the allowedFolder
-        if item == allowedFolder:
-            continue  # Skip the allowedFolder
-        
-        # If item is a directory, remove it and its contents
-        if os.path.isdir(item_path):
-            shutil.rmtree(item_path)
-            print(f"Removed directory: {item_path}")
-        # If item is a file, remove it
-        else:
-            os.remove(item_path)
-            print(f"Removed file: {item_path}")
 
 ######v2
 def get_repo_user_and_name(module_path):
