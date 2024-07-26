db39c7a418ba2f7e3aa0c56d2563a8775e224071
Weixuan Fu <weixuanf@outlook.com>
2024-07-26 19:41:56 +0800

    add inject css method to import css to html

--- web/extensions/workspace-manager/src/main.tsx
+++ web/extensions/workspace-manager/src/main.tsx
@@ -5,9 +5,12 @@ import "./index.css";
 const topbar = document.createElement("div");
 document.body.append(topbar);
 import App from "./App";
+import { injectCSS } from "./injectCss";
 
 ReactDOM.createRoot(topbar).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+injectCSS("/web/extensions/workspace-manager/dist/input.css");
--- web/extensions/workspace-manager/vite.config.ts
+++ web/extensions/workspace-manager/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -15,14 +14,8 @@ export default defineConfig(({ mode }) => ({
     rollupOptions: {
       // externalize deps that shouldn't be bundled into your library
       external: ["/scripts/app.js", "/scripts/api.js"],
-      // input: {
-      //   input: "/src/main.tsx",
-      // },
-      lib: {
-        format: ["es"],
-        entry: {
-          index: "src/main.tsx",
-        },
+      input: {
+        input: "/src/main.tsx",
       },
       output: {
         // Provide global variables to use in the UMD build for externalized deps
@@ -31,14 +24,11 @@ export default defineConfig(({ mode }) => ({
         },
         dir: "./dist",
         // assetFileNames: "[name]-[hash][extname]",
-        // entryFileNames: "[name].js",
-        // chunkFileNames: `[name]-[hash].js`,
-        chunkFileNames: "chunks/[name].[hash].js",
-        // Put chunk styles at <output>/assets
-        assetFileNames: "assets/[name][extname]",
         entryFileNames: "[name].js",
+        chunkFileNames: `[name]-[hash].js`,
+        assetFileNames: `[name].[ext]`,
       },
     },
   },
-  plugins: [react(), libInjectCss()],
+  plugins: [react()],
 }));
