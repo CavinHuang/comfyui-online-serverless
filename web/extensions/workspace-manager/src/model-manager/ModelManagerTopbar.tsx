4785f314f264fc913d8afdbbae7ee5b4e5d01fb1
Weixuan Fu <weixuanf@outlook.com>
2024-07-26 20:05:06 +0800

    add shadcn

--- web/extensions/workspace-manager/src/model-manager/ModelManagerTopbar.tsx
+++ web/extensions/workspace-manager/src/model-manager/ModelManagerTopbar.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { app } from "../comfyapp";
+import { Button } from "@/components/ui/button";
 
 export default function ModelManagerTopbar() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">
-        models
-      </h1>
+      <Button onClick={() => app.modelManager.createModel()}>Models</Button>
     </div>
   );
 }
